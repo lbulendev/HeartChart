6a7411e366c57424d0e9b6f0de3b3511f912bca3
Larry Bulen <larry@nearchus.com>
2018-08-11 13:38:51 -0700

    new iOS project

---
+++ ios-Swift/HeartChart-iOS/HeartChart-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartChart-iOS
+//
+//  Created by Larry Bulen on 8/11/18.
+//  Copyright © 2018 Larry Bulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
